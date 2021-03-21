24285d7d5507cd4eed1888c8a7fe768c60fe926c
bentaylor <ben10taylor10@hotmail.com>
2021-03-21 15:16:17 +0000

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
-import React, { useContext,} from 'react';
+import React, { useContext, useEffect} from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { motion } from "framer-motion"
+import axios from "axios"
 import "../styles/home/home.css"
 import {FETCH_TOURNAMENTS_QUERY} from "../utils/queries"
 
@@ -13,6 +14,25 @@ import {stagger} from "../utils/animations"
 function Home() {
 
     const { user } = useContext(AuthContext);
+
+    useEffect(async () => {
+        // const options = {
+        //     headers: {
+        //         "x-phantombuster-key": "V9pUmR9qEzvW3p88FhEJ6AfQIq39lYphKy3CIqj51eE",
+        //         "Content-Type": "application/json",
+        //     },
+        // }
+
+        // const res = await axios.post(
+        //         "https://api.phantombuster.com/api/v1/agent/8544549666691668/launch",
+        //         {"output":"result-object","argument":{"sessionCookie":"AQEDATFGnIsFoMCrAAABeFUmeqMAAAF4eTL-o00AKaf4Ox31kN0J_jFKduoA38UFVgcho-qetOwqjszGQtOMzloD5CKWw_zHk1QVgvpQDrPDb6VfJJ7rA-U9SnLAfDLefX2W9J9yXKUDhqIV-ydFN_yR","searchUrl":"https://www.linkedin.com/search/results/all/?keywords=technology&origin=GLOBAL_SEARCH_HEADER","emailChooser":"phantombuster"}},
+        //         options,
+        //     )
+            
+
+        //     console.log(res.data)
+	
+    }, [])
 
     const { loading,error, data: { getTournaments: tournaments } = {}} = useQuery(FETCH_TOURNAMENTS_QUERY, {
         variables: {username: user?.username}
